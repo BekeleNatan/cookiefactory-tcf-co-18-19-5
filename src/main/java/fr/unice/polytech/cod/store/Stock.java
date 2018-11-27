393334525c9314b839a79df41ca082bd6ff8f4be
TOGNI Tetevi Fiacre <tognitete@gmail.com>
2018-11-27 22:31:12 +0100

    Store definition

--- src/main/java/fr/unice/polytech/cod/store/Stock.java
+++ src/main/java/fr/unice/polytech/cod/store/Stock.java
@@ -8,21 +8,79 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Stock {
-	public HashMap<Ingredient,Integer> ingredients = new HashMap<Ingredient, Integer>();
+	public HashMap<Ingredient,Integer> ingredients = new HashMap<>();
 
 	public void addIngredient(Ingredient aIngredient, int quantity) {
-		throw new UnsupportedOperationException();
+
+		if(aIngredient != null & quantity > 0){
+
+			 this.ingredients.put(aIngredient,quantity);
+		}
 	}
 
-	public void removeIngredient(Ingredient aIngredient, int quantity) {
-		throw new UnsupportedOperationException();
+	public  boolean removeIngredient(Ingredient aIngredient, int quantity) {
+
+		boolean isPossible = false;
+
+		if(aIngredient!= null & quantity > 0){
+
+			 int oldQuantity = this.ingredients.get(aIngredient);
+
+			 int newQuantite =  oldQuantity - quantity ;
+
+			 if(newQuantite >= 0 ) {
+
+			 	  this.ingredients.replace(aIngredient,newQuantite);
+
+			 	  isPossible = true;
+			 }else{
+
+			 	System.out.println("Le stock est insuffisant pour satisfaire cette commande");
+			 	 isPossible = false;
+			 }
+
+
+		     if(newQuantite == 0 ){
+
+		     	System.out.println("Vous venez d'épuiser cet ingredient.Penser à vous approvisionner");
+			 }
+
+			 if(newQuantite < 25) {
+
+			 	System.out.println("Cet ingrédient tand à s'épuiser.Penser à vous approvisionner");
+			 }
+
+		}
+
+		return isPossible;
 	}
 
 	public Ingredient getIngredientByName(String aNomIngredient) {
-		throw new UnsupportedOperationException();
+
+		Ingredient ingredientObject = null;
+
+		for (Ingredient ingredient  : this.ingredients.keySet()){
+
+			 if(ingredient.getName().equals(aNomIngredient)){
+
+			 	 ingredientObject = ingredient;
+			 }
+		}
+
+		return ingredientObject;
 	}
 
 	public List<Ingredient> getIngredientsByType(IngredientType aIngredientType) {
-		throw new UnsupportedOperationException();
+
+		List<Ingredient> ingredientByType = new ArrayList<>();
+		for (Ingredient ingredient  : this.ingredients.keySet()){
+
+			if(ingredient.getType().equals(aIngredientType)){
+
+				ingredientByType.add(ingredient);
+			}
+		}
+
+		return ingredientByType;
 	}
 }
