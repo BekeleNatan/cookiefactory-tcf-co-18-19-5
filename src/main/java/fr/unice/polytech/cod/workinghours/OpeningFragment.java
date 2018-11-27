8701fa421a4b49a0ee12027b35d39282d59296b7
Walid LARABI <walid.larabi@etu.unice.fr>
2018-11-27 13:44:54 +0100

    Fix #13 : Restoring WorkingHours and their unit tests

--- src/main/java/fr/unice/polytech/cod/workinghours/OpeningFragment.java
+++ src/main/java/fr/unice/polytech/cod/workinghours/OpeningFragment.java
@@ -4,7 +4,30 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public class OpeningFragment {
-	private DayOfWeek day;
-	private LocalTime opening;
-	private LocalTime closing;
+    private DayOfWeek day;
+    private LocalTime opening;
+    private LocalTime closing;
+
+    public OpeningFragment(DayOfWeek day, LocalTime opening, LocalTime closing) throws RuntimeException {
+        this.day = day;
+
+        if (opening.isAfter(closing)) {
+            throw new RuntimeException("Opening time must be after closing time");
+        } else {
+            this.opening = opening;
+            this.closing = closing;
+        }
+    }
+
+    DayOfWeek getDay() {
+        return day;
+    }
+
+    LocalTime getOpening() {
+        return opening;
+    }
+
+    LocalTime getClosing() {
+        return closing;
+    }
 }
--- src/main/java/fr/unice/polytech/cod/workinghours/WorkingHours.java
+++ src/main/java/fr/unice/polytech/cod/workinghours/WorkingHours.java
@@ -5,27 +5,64 @@ import fr.unice.polytech.cod.store.Store;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.List;
 
 public class WorkingHours {
-	public ArrayList<OpeningFragment> openingFragments = new ArrayList<OpeningFragment>();
+    private List<OpeningFragment> openingFragments;
 
-	public boolean isOpenOn(DayOfWeek aDay, LocalTime aAt) {
-		throw new UnsupportedOperationException();
-	}
+    WorkingHours() {
+        openingFragments = new ArrayList<>();
+    }
 
-	public boolean addOpeningTime(DayOfWeek aDay, LocalTime aFrom, LocalTime aTo) {
-		throw new UnsupportedOperationException();
-	}
+    // ok
+    private boolean inBetween(LocalTime actual, LocalTime from, LocalTime to) {
+        return !actual.isBefore(from) && actual.isBefore(to);  // inclusive beginning but exclusive ending (half-open)
+    }
 
-	public boolean deleteOpeningTime(DayOfWeek aDay, LocalTime aFrom, LocalTime aTo) {
-		throw new UnsupportedOperationException();
-	}
+    // ok
+    public boolean addOpeningFragement(DayOfWeek day, LocalTime from, LocalTime to) throws RuntimeException {
+        OpeningFragment openingFragment = new OpeningFragment(day, from, to);
+        for (OpeningFragment of : this.openingFragments) {
+            if (of.getDay().equals(openingFragment.getDay())) {
+                if (inBetween(openingFragment.getOpening(), of.getOpening(), of.getClosing())) {
+                    throw new RuntimeException("You can't add an Opening Fragment that overlaps another one");
+                }
+                this.openingFragments.add(openingFragment);
+                return true;
+            }
+        }
+        this.openingFragments.add(openingFragment);
+        return true;
+    }
 
-	public boolean addRestrictedHoursDay(DayOfWeek aDay, OpeningFragment aOpeningFragment) {
-		throw new UnsupportedOperationException();
-	}
+    // ok
+    public boolean deleteOpeningFragement(DayOfWeek day, LocalTime from, LocalTime to) {
+        for(OpeningFragment of : this.openingFragments){
+            if(of.getDay().equals(day) && of.getOpening().equals(from) && of.getClosing().equals(to)){
+                this.openingFragments.remove(of);
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public boolean deleteRestrictedHoursDay(DayOfWeek aDay, OpeningFragment aOpeningFragment) {
-		throw new UnsupportedOperationException();
-	}
+    // ok
+    public boolean isOpenOn(DayOfWeek day, LocalTime at) {
+        for (OpeningFragment of : this.openingFragments){
+            if(day.equals(of.getDay()) && (inBetween(at, of.getOpening(), of.getClosing()))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // todo WorkingHours : adding restrictedHoursDay
+    public boolean addRestrictedHoursDay(DayOfWeek aDay, OpeningFragment aOpeningFragment) {
+        throw new UnsupportedOperationException();
+    }
+
+    // todo WorkingHours : delete restrictedHoursDay
+    public boolean deleteRestrictedHoursDay(DayOfWeek aDay, OpeningFragment aOpeningFragment) {
+        throw new UnsupportedOperationException();
+    }
 }
