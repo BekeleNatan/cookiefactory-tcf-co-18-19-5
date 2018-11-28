fa76b159e458408b31afe759954ec78fb52f0a53
Walid LARABI <walid.larabi@etu.unice.fr>
2018-11-28 11:30:06 +0100

    fix #26 : Unit tests ingredients

--- src/test/java/fr/unice/polytech/cod/recipe/ingredients/IngredientTest.java
+++ src/test/java/fr/unice/polytech/cod/recipe/ingredients/IngredientTest.java
@@ -1,30 +1,31 @@
 package fr.unice.polytech.cod.recipe.ingredients;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class IngredientTest {
 
-    // TODO : IMPLEMENTS INGREDIENT TESTS
+    private static final double DELTA = 1e-15;
+    private Ingredient ingredient;
 
+    @Before
+    public void initialize(){
+        ingredient = new Ingredient("M&Ms", IngredientType.Topping, 2, 1);
+    }
     @Test
     public void getName() {
+        assertEquals("M&Ms", ingredient.getName());
     }
 
     @Test
     public void getType() {
+        assertEquals(IngredientType.Topping, ingredient.getType());
     }
 
     @Test
     public void getPricePerUnit() {
-    }
-
-    @Test
-    public void setIngredientPrice() {
-    }
-
-    @Test
-    public void setPriceMargin() {
+        assertEquals(3, ingredient.getPricePerUnit(), DELTA);
     }
 }
