743588e764f54d4b57dd83e41297e950af5a7964
bonnet <theo.bonnet1@etu.unice.fr>
2018-12-04 09:46:41 +0100

    add method to prepare refund methods

--- src/main/java/fr/unice/polytech/cod/payment/unfaithpass/UnfaithPassMoney.java
+++ src/main/java/fr/unice/polytech/cod/payment/unfaithpass/UnfaithPassMoney.java
@@ -5,4 +5,9 @@ public class UnfaithPassMoney extends UnfaithPassPayment {
 	public boolean pay(Double aAmount) {
 		throw new UnsupportedOperationException();
 	}
+
+	@Override
+	public boolean refund(Double aAmount) {
+		return false; // todo
+	}
 }
