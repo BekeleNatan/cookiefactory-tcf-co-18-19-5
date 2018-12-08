a81dc500307aae7062136a500affdcbad6b8d916
TOGNI Tetevi Fiacre <tognitete@gmail.com>
2018-12-08 21:37:52 +0100

    fix #15 : Create recipe(gherkin code)

---
+++ src/test/java/fr/unice/polytech/cod/order/ItemTest.java
@@ -0,0 +1,30 @@
+package fr.unice.polytech.cod.order;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+
+
+    @Test
+    public void getQuantity() {
+    }
+
+    @Test
+    public void addQuantity() {
+    }
+
+    @Test
+    public void setQuantity() {
+    }
+
+    @Test
+    public void getRecipe() {
+    }
+
+    @Test
+    public void getPrice() {
+    }
+}
